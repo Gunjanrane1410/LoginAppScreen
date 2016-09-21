7225cdbd72003f19927614acf1220f830485c93f
Student P_04 <studentp_04@student-p-04.local>
2016-09-21 16:14:29 +0530

    Initial Commit

---
+++ LoginAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginAPP
+//
+//  Created by Student P_04 on 21/09/16.
+//  Copyright © 2016 Gunjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoginAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginAPP
+//
+//  Created by Student P_04 on 21/09/16.
+//  Copyright © 2016 Gunjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
